10030a92aad3407996329fd7d0160487b101e7fe
churl <tobi@urpost.de>
2022-01-21 00:26:50 +0100

    update launch script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d --env-file .env --mount src=/root/HeidiBot/voicelines,target=/sounds,type=bind --name heidibot registry.gitlab.com/churl/heidibot
+docker run -d --env-file .env --mount src=/home/christoph/HeidiBot/voicelines,target=/sounds,type=bind --name heidibot registry.gitlab.com/churl/heidibot
