d9338da50a5d2ddf99f1d23003b54cc13dcf42f7
churl <tobi@urpost.de>
2022-01-24 22:04:58 +0100

    launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git pull
+
 docker pull registry.gitlab.com/churl/heidibot
 docker container rm -f heidibot
 docker run -d --env-file /home/christoph/HeidiBot/.env --mount src=/home/christoph/HeidiBot/voicelines,target=/sounds,type=bind --name heidibot registry.gitlab.com/churl/heidibot
