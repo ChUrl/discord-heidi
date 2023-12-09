0f6cc121824fe0a02a39f928641183517d8f145e
Christoph Urlacher <christoph.urlacher@protonmail.com>
2023-12-09 17:36:53 +0100

    Delete orphaned code

--- launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd /home/christoph/HeidiBot
-git pull
-
-docker pull registry.gitlab.com/churl/heidibot
-docker container rm -f heidibot
-docker run -d --env-file /home/christoph/HeidiBot/.env --mount src=/home/christoph/HeidiBot/voicelines,target=/sounds,type=bind --name heidibot registry.gitlab.com/churl/heidibot
-docker image prune -f
